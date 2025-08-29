9b92e71afdc6ff02c48faeeb64b9388c11ee1e40
18660801526 <18660801526@163.com>
2025-08-29 12:25:54 +0800

    支持Qwen Image

--- layout.py
+++ layout.py
@@ -6,10 +6,10 @@ active_menu = "dashboard"
 menu_items = [
     {"label": "关于LoRA训练大师", "icon": "rocket", "key": "/"},
     {"label": "万相视频 LoRA训练", "icon": "rocket", "key": "/Wan"},
-    {"label": "混元视频 LoRA训练", "icon": "rocket", "key": "/HunyuanVideo"},
-    {"label": "FramePack LoRA训练", "icon": "download", "key": "/FramePack"},
     {"label": "Flux Kontext LoRA训练", "icon": "settings", "key": "/FluxKontext"},
     {"label": "Qwen Image LoRA训练", "icon": "rocket", "key": "/QwenImage"},
+    {"label": "混元视频 LoRA训练", "icon": "rocket", "key": "/HunyuanVideo"},
+    {"label": "FramePack LoRA训练", "icon": "download", "key": "/FramePack"},
     {"label": "TensorBoard 仪表盘", "icon": "rocket", "key": "/Tensorboard"},
 ]
 
